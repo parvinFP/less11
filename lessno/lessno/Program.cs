99a11c4244a9e0d5808143c145b3c12c1a2d5d5d
[parvinFP] <[parvinfp@code.edu.az]>
2021-12-13 22:38:29 +0300

    less 11

---
+++ lessno/lessno/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using librarycalculator;
+Calculator calculator=new Calculator();
+
+int toplama = calculator.toplama(7, 8);
+Console.Write("iki ededin toplamasi:="   +toplama+ "\n");
+
+int cixma=calculator.cixma(10,7);
+Console.Write("iki ededin cixmasi:="   + cixma + "\n");
+
+double bolme = calculator.bolme(8, 4);
+Console.Write("iki ededin bolunmesi:="  +bolme + "\n");
+
+int vurma=calculator.vurma(8, 4);
+Console.Write("iki ededin vurma:="+ vurma + "\n");
+
+
---
+++ lessno/librarycalculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace librarycalculator
+{
+    public class Calculator
+    {
+       public   int toplama(int num1,int num2)
+        {
+            return num1 + num2;   
+        }
+        public  int cixma(int num1,int num2)
+        {
+            return num1 - num2;
+        }
+        public double bolme(double num1,double num2)
+        {
+            return (num1 / num2);
+        
+        }
+        public int vurma(int num1,int num2)
+        {
+            return num1 * num2;
+        }
+        
+
+    }
+}
